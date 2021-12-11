644f61781ddd0fce9ecbe2c532836e03e58dc0cf
Daniel-Cas <dmattcomics@gmail.com>
2021-12-11 00:16:32 -0500

    Add catchErrors

--- src/app/clientes/clientes.service.ts
+++ src/app/clientes/clientes.service.ts
@@ -1,9 +1,11 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
 
 import { environment } from '../../environments/environment'
 import { Cliente } from '../interfaces/cliente';
+import { MessageService } from 'primeng/api';
+import { Router } from '@angular/router';
 
 
 
@@ -16,26 +18,78 @@ export class ClientesService {
 
   private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+              private messageService : MessageService,
+              private router         : Router) { }
 
   getClientes(): Observable<Cliente[]>{
-    return this.http.get<Cliente[]>(`${ this.url }/clientes`);
+    return this.http.get<Cliente[]>(`${ this.url }/clientes`)
+    .pipe(
+      catchError( e =>{
+
+        console.error(e.error.mensaje);
+        this.messageService.add({sticky:true, severity:'error', summary: 'Error al obtener los clientes', detail: e.error.mensaje});
+        setTimeout(() => {this.router.navigate(['/clientes'])} , 2500);
+        return throwError(e);
+
+      })
+    )
   }
 
   create(cliente: Cliente){
-    return this.http.post<Cliente>(`${this.url }/clientes`, cliente);
+    return this.http.post<Cliente>(`${this.url }/clientes`, cliente).pipe(
+      catchError( e =>{
+
+        console.error(e.error.mensaje);
+        this.messageService.add({sticky:true, severity:'error', summary: 'Error al crear', detail: e.error.mensaje});
+        setTimeout(() => {this.router.navigate(['/clientes'])} , 2500);
+        return throwError(e);
+
+      })
+    )
   }
 
   getCliente(id : any): Observable<Cliente>{
-    return this.http.get<Cliente>(`${this.url}/clientes/${id}`);
+    return this.http.get<Cliente>(`${this.url}/clientes/${id}`).pipe(
+      catchError( e => {
+
+        console.error(e.error.mensaje);
+        this.messageService.add({sticky:true, severity:'error', summary: 'Error al editar el cliente', detail: e.error.mensaje});
+        setTimeout(() => {this.router.navigate(['/clientes'])} , 2500);
+        return throwError(e);
+
+
+      })
+    )
   }
 
   update(cliente: Cliente): Observable<Cliente>{
-    return this.http.put<Cliente>(`${this.url}/clientes/${cliente.id}`, cliente, ({headers: this.httpHeaders}));
+    return this.http.put<Cliente>(`${this.url}/clientes/${cliente.id}`, cliente, ({headers: this.httpHeaders}))
+    .pipe( 
+      catchError( e => {
+
+        console.error(e.error.mensaje);
+        this.messageService.add({sticky:true, severity:'error', summary: 'Error al editar el cliente', detail: e.error.mensaje});
+        setTimeout(() => {this.router.navigate(['/clientes'])} , 2500);
+        return throwError(e);
+
+
+      })
+    )
   }
 
   delete(id: number): Observable<Cliente>{
-    return this.http.delete<Cliente>(`${this.url}/clientes/${id}`, {headers: this.httpHeaders});
+    return this.http.delete<Cliente>(`${this.url}/clientes/${id}`, {headers: this.httpHeaders})
+    .pipe( 
+      catchError( e => {
+
+        console.error(e.error.mensaje);
+        this.messageService.add({sticky:true, severity:'error', summary: 'Error al eliminar el cliente', detail: e.error.mensaje});
+        setTimeout(() => {this.router.navigate(['/clientes'])} , 2500);
+        return throwError(e);
+
+
+      }));
   }
 
 }
